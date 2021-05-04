8218e7e623aabe7436a46e99421e0cfb840efbd3
awe4 <alec.eickbusch@yale.edu>
2021-05-04 10:51:20 -0400

    stop loss 1e-6

--- tomography/density_matrix_reconstruction.py
+++ tomography/density_matrix_reconstruction.py
@@ -140,7 +140,7 @@ def reconstruct_state_wigner(normalized_W_data, alphas_I, alphas_Q, N=7, N_large
 
     i = 0
     max_iter = 2000
-    stop_loss = 1e-7
+    stop_loss = 1e-6
     while i < max_iter:
         optimizer.minimize(problem, var_list=[A, B])
         if i % 100 == 0:
@@ -233,7 +233,7 @@ def reconstruct_state_cf(normalized_cf_data, betas_I, betas_Q=None, N=7, N_large
 
     i = 0
     max_iter = 2000
-    stop_loss = 1e-7
+    stop_loss = 1e-6
     while i < max_iter:
         optimizer.minimize(problem, var_list=[A, B])
         if i % 100 == 0:
